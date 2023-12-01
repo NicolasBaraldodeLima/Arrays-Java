e23672648c0c86aa2168eb044af57780c43630c8
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-12-01 15:10:28 -0300

    Add files via upload

---
+++ CursoJava.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.introducao;
+
+
+public class CursoJava {
+
+    public static void main(String[] args) {
+
+        int[] num = new int[5];
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        num[0] = 10;
+        num[1] = 5;
+        num[2] = 15;
+        num[3] = 0;
+        num[4] = 8;
+
+
+
+        System.out.printf("%d", num[1], numeros[1]);
+    }
+
+
+
+    }
+
+
+
